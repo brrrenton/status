92ea94724a25d6c7ee6872969a918c314e340b2a
Brenton Leighton <brenton.leighton@uts.edu.au>
2019-10-23 13:54:31 +1100

    Use Ip4Config to get the address of a WiFi hotspot

--- network.py
+++ network.py
@@ -6,12 +6,6 @@ NM_DEVICE_TYPE_WIFI = 2
 NM_STATE_ACTIVATED = 100
 
 
-def format_ip4_address(value):
-    tmp = str(ipaddress.IPv4Address(value)).split(".")
-    tmp.reverse()
-    return ".".join(tmp)
-
-
 def format_bps(value):
     if value >= 1000:
         return str(int(value / 1000)) + " Gbit/s"
@@ -43,14 +37,16 @@ def networks(spacer):
                 string += spacer
 
             if device_proxy.DeviceType == NM_DEVICE_TYPE_ETHERNET:
-                string += device_proxy.Interface + ": " + format_ip4_address(
-                    device_proxy.Ip4Address) + " (" + format_bps(device_proxy.Speed) + ")"
+                ip4_config_proxy = system_bus.get('org.freedesktop.NetworkManager', device_proxy.Ip4Config)
+                string += device_proxy.Interface + ": " + ip4_config_proxy.AddressData[0][
+                    "address"] + " (" + format_bps(device_proxy.Speed) + ")"
 
             elif device_proxy.DeviceType == NM_DEVICE_TYPE_WIFI:
+                ip4_config_proxy = system_bus.get('org.freedesktop.NetworkManager', device_proxy.Ip4Config)
                 active_access_point_proxy = system_bus.get('org.freedesktop.NetworkManager',
                                                            device_proxy.ActiveAccessPoint)
 
-                string += device_proxy.Interface + ": " + format_ip4_address(
-                    device_proxy.Ip4Address) + " (" + format_ssid(active_access_point_proxy.Ssid) + ")"
+                string += device_proxy.Interface + ": " + ip4_config_proxy.AddressData[0][
+                    "address"] + " (" + format_ssid(active_access_point_proxy.Ssid) + ")"
 
     return string

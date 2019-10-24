602c106b045090830d16e917f16144a1a80ddca4
Brenton Leighton <brenton.leighton@uts.edu.au>
2019-10-25 09:48:04 +1100

    Clean up code

--- benchmark.py
+++ benchmark.py
@@ -1,37 +1,9 @@
 #!/usr/bin/python3
 
-# requires "python3-pydbus" and "python3-xlib"
-
-import datetime
-
-from pydbus import SystemBus
-
-from network import Network
-from memory import memory_free
-from battery import Battery
-
-SPACER = " | "
-
-system_bus = SystemBus()
-
-network = Network(system_bus)
-battery = Battery(system_bus)
-
-
-def f():
-    string = "".join([" ",
-                      network.ethernet(SPACER),
-                      network.wifi(SPACER),
-                      memory_free(SPACER),
-                      battery.status(SPACER),
-                      datetime.datetime.now().strftime("%Y-%m-%d %A %-I:%M %P"),
-                      " "])
-
-    print(string)
-
+from status import status
 
 if __name__ == "__main__":
     import timeit
 
     n = 10
-    print(timeit.timeit("f()", globals=globals(), number=n) / n)
+    print(timeit.timeit("status()", globals=globals(), number=n) / n)
--- status.py
+++ status.py
@@ -2,7 +2,6 @@
 
 # requires "python3-pydbus" and "python3-xlib"
 
-import time
 import datetime
 
 from pydbus import SystemBus
@@ -11,11 +10,6 @@ from network import Network
 from memory import memory_free
 from battery import Battery
 
-import Xlib.display
-
-display = Xlib.display.Display()
-root = display.screen().root
-
 SPACER = " | "
 
 system_bus = SystemBus()
@@ -23,15 +17,26 @@ system_bus = SystemBus()
 network = Network(system_bus)
 battery = Battery(system_bus)
 
-while 1:
-    string = "".join([" ",
-                      network.ethernet(SPACER),
-                      network.wifi(SPACER),
-                      memory_free(SPACER),
-                      battery.status(SPACER),
-                      datetime.datetime.now().strftime("%Y-%m-%d %A %-I:%M %P"),
-                      " "])
 
-    root.set_wm_name(string)
-    display.sync()
-    time.sleep(1)
+def status():
+    return "".join([" ",
+                    network.ethernet(SPACER),
+                    network.wifi(SPACER),
+                    memory_free(SPACER),
+                    battery.status(SPACER),
+                    datetime.datetime.now().strftime("%Y-%m-%d %A %-I:%M %P"),
+                    " "])
+
+
+if __name__ == "__main__":
+    import time
+
+    import Xlib.display
+
+    display = Xlib.display.Display()
+    root = display.screen().root
+
+    while 1:
+        root.set_wm_name(status())
+        display.sync()
+        time.sleep(1)
